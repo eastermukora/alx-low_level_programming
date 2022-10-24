55d46876e7e89b26e0f4a07491b6f44724e4889c
holiday <eastermukora@gmail.com>
2022-10-24 16:15:23 -0700

    isupper program push one

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,14 +9,15 @@
 int _isupper(int c); //prototype of the functions
 int main(void)
 {
-    char c;
-    if(c = 'A')
-      {
-	printf("%c: %d\n", c, _isupper(c));
-      }
-    else if(c = 'a' && c<='z')
-      {
-	printf("%c: %d\n", c, _isupper(c));
-      }
-    return (0);
+  char c;
+  if(c = 'A')
+    {
+      printf("%c: %d\n", c, _isupper(c));
+    }
+  return (1);
+  else if(c = 'a' && c<='z')
+    {
+      printf("%c: %d\n", c, _isupper(c));
+    }
+  return (0);
 }
