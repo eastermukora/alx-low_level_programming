4731344aeb2f6d5d1823763e7d928a3c5fbebf60
holiday <eastermukora@gmail.com>
2022-10-24 16:06:38 -0700

    isupper program push 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,3 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c); //prototype of the functions
+int main(void)
+{
+    char c;
+    if(c = 'A')
+      {
+	printf("%c: %d\n", c, _isupper(c));
+      }
+    else if(c = 'a' && c<='z')
+      {
+	printf("%c: %d\n", c, _isupper(c));
+      }
+    return (0);
+}
