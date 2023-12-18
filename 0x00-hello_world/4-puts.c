183104e779be7432223f7e19af181300d51da3cd
eastermukora <eastermukora@gmail.com>
2023-12-18 10:22:55 -0800

    4-puts.c betty linting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ *this function prints a text a string into the output
+ *using the main function puts
+Return: 0;
+ */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
