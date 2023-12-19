b16caa9836331b78c4c30fe7ccb0f650ed1d841d
eastermukora <eastermukora@gmail.com>
2023-12-19 05:05:18 -0800

    5printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - prints a string into the output
+*
+*using the main function
+*this program prints out
+*'with proper grammar, but the outcome is a piece of art,'
+*Return: 0;
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
